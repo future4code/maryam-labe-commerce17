1beada9baac93db6d012bdf22ba334b9bbb1d09d
Stephanie Flavio <stephdpmello@gmail.com>
2021-09-17 14:07:15 -0300

    ajustei os cards no projetos

--- src/components/Home/listaDeProdutos.js
+++ src/components/Home/listaDeProdutos.js
@@ -1,37 +1,46 @@
+import meteorito1 from '../imagens-meteoritos/meteorito-azul.png'
+import meteorito2 from '../imagens-meteoritos/meteorito-bruto.png'
+import meteorito3 from '../imagens-meteoritos/meteorito-verde.png'
+import meteorito4 from '../imagens-meteoritos/meteorito-vermelho.png'
+import meteorito5 from '../imagens-meteoritos/meteorito-rosa.png'
+import meteorito6 from '../imagens-meteoritos/meteorito-amarelo.png'
+
+
+
 export const listaDeProdutos = [
     {
         id: 1,
-        imagemUrl: "https://www.cnnbrasil.com.br/wp-content/uploads/2021/06/12312_DBB283E3B167EA2E.jpg",
+        imagemUrl: meteorito1,
         nome: "meteorito azul",
         preco: 530
     },
     {
         id: 2,
-        imagemUrl: "https://www.cnnbrasil.com.br/wp-content/uploads/2021/06/12312_DBB283E3B167EA2E.jpg",
+        imagemUrl: meteorito2,
         nome: "meteorito rochoso",
         preco: 80
     },
     {
         id: 3,
-        imagemUrl: "https://www.cnnbrasil.com.br/wp-content/uploads/2021/06/12312_DBB283E3B167EA2E.jpg",
-        nome: "meteorito roxo",
+        imagemUrl: meteorito3,
+        nome: "meteorito verde",
         preco: 850
     },
     {
         id: 4,
-        imagemUrl: "https://www.cnnbrasil.com.br/wp-content/uploads/2021/06/12312_DBB283E3B167EA2E.jpg",
+        imagemUrl: meteorito4,
         nome: "meteorito vermelho",
         preco: 610
     },
     {
         id: 5,
-        imagemUrl: "https://www.cnnbrasil.com.br/wp-content/uploads/2021/06/12312_DBB283E3B167EA2E.jpg",
+        imagemUrl: meteorito5,
         nome: "meteorito rosa",
         preco: 790
     },
     {
         id: 6,
-        imagemUrl: "https://www.cnnbrasil.com.br/wp-content/uploads/2021/06/12312_DBB283E3B167EA2E.jpg",
+        imagemUrl: meteorito6,
         nome: "meteorito laranja",
         preco: 420
     }
