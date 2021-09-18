7c683887d385f0f72bbe1c55293b03b655ed75a2
Mariana Carolina de Souza <smcarolina42@gmail.com>
2021-09-18 17:23:41 -0300

    mudanças de layout

--- src/App.js
+++ src/App.js
@@ -11,14 +11,36 @@ import meteorito3 from './components/imagens-meteoritos/meteorito-verde.png'
 import meteorito4 from './components/imagens-meteoritos/meteorito-vermelho.png'
 import meteorito5 from './components/imagens-meteoritos/meteorito-rosa.png'
 import meteorito6 from './components/imagens-meteoritos/meteorito-amarelo.png'
+import backgroud from './imagens/background.png'
+import logo from './imagens/logo.png'
 
 
 const AppGrid = styled.div`
-display: grid;
-grid-template-columns: 1fr 3fr 1fr;
-
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  position: absolute;
+  top:10px;
+  left: 8px;
+  
 
 `
+
+const Background = styled.img`
+  width: 100%;
+  height: 100%;
+`
+
+const Footer = styled.footer`
+    grid-column: 1/4;
+    /* width: 100vw; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 0.3rem;
+    margin-bottom: 0.5rem;
+`
+
 
 class App extends React.Component {
 
@@ -28,44 +50,44 @@ class App extends React.Component {
     precoMaximo: "",
     ordenacao: "maiorPreco",
     contador: 0,
-    produtos : [
+    produtos: [
       {
         id: 1,
         imagemUrl: meteorito1,
         nome: "meteorito azul",
         preco: 530
-    },
-    {
+      },
+      {
         id: 2,
         imagemUrl: meteorito2,
         nome: "meteorito rochoso",
         preco: 80
-    },
-    {
+      },
+      {
         id: 3,
         imagemUrl: meteorito3,
         nome: "meteorito verde",
         preco: 850
-    },
-    {
+      },
+      {
         id: 4,
         imagemUrl: meteorito4,
         nome: "meteorito vermelho",
         preco: 610
-    },
-    {
+      },
+      {
         id: 5,
         imagemUrl: meteorito5,
         nome: "meteorito rosa",
         preco: 790
-    },
-    {
+      },
+      {
         id: 6,
         imagemUrl: meteorito6,
         nome: "meteorito laranja",
         preco: 420
-    }
-  ]
+      }
+    ]
 
   }
 
@@ -88,43 +110,54 @@ class App extends React.Component {
   adicionarAoCarrinho = (event) => {
     this.setState({ contador: event.target.value })
     console.log(event)
-}
+  }
 
   render() {
     return (
-      <AppGrid>
+      <div className="page-container">
+        <Background src={backgroud} className="background" />
 
-        <Filtro
-          busca={this.state.busca}
-          atualizaBusca={this.atualizaBusca}
-          precoMinimo={this.state.precoMinimo}
-          atualizaPrecoMinimo={this.atualizaPrecoMinimo}
-          precoMaximo={this.state.precoMaximo}
-          atualizaPrecoMaximo={this.atualizaPrecoMaximo}
-          ordenacao={this.state.ordenacao}
-          ordenaProdutos={this.ordenaProdutos}
-          produtos={this.state.produtos}
-        />
 
-        <Main
-          busca={this.state.busca}
-          atualizaBusca={this.atualizaBusca}
-          precoMinimo={this.state.precoMinimo}
-          atualizaPrecoMinimo={this.atualizaPrecoMinimo}
-          precoMaximo={this.state.precoMaximo}
-          atualizaPrecoMaximo={this.atualizaPrecoMaximo}
-          ordenacao={this.state.ordenacao}
-          ordenaProdutos={this.ordenaProdutos}
-          produtos={this.state.produtos}
-          adicionarAoCarrinho={this.adicionarAoCarrinho}
-        />
-        <Carrinho produtos={this.state.produtos}/>
-        
-      </AppGrid>
+        <AppGrid>
 
+          <Filtro
+            busca={this.state.busca}
+            atualizaBusca={this.atualizaBusca}
+            precoMinimo={this.state.precoMinimo}
+            atualizaPrecoMinimo={this.atualizaPrecoMinimo}
+            precoMaximo={this.state.precoMaximo}
+            atualizaPrecoMaximo={this.atualizaPrecoMaximo}
+            ordenacao={this.state.ordenacao}
+            ordenaProdutos={this.ordenaProdutos}
+            produtos={this.state.produtos}
+          />
+
+          <Main
+            busca={this.state.busca}
+            atualizaBusca={this.atualizaBusca}
+            precoMinimo={this.state.precoMinimo}
+            atualizaPrecoMinimo={this.atualizaPrecoMinimo}
+            precoMaximo={this.state.precoMaximo}
+            atualizaPrecoMaximo={this.atualizaPrecoMaximo}
+            ordenacao={this.state.ordenacao}
+            ordenaProdutos={this.ordenaProdutos}
+            produtos={this.state.produtos}
+            adicionarAoCarrinho={this.adicionarAoCarrinho}
+          />
+          <Carrinho produtos={this.state.produtos} />
+          <Footer className="footer">
+            <p>Conheça nossas redes sociais</p>
+            <div>
+              <img src="https://cdn-icons-png.flaticon.com/512/185/185985.png" alt="" />
+              <img src="https://cdn-icons-png.flaticon.com/512/185/185961.png" alt="" />
+              <img src="https://img-premium.flaticon.com/png/512/1377/premium/1377223.png?token=exp=1631977730~hmac=528eca751a912ec1e21a8e29c9c84adb" alt="" />
+            </div>
+          </Footer>
+        </AppGrid>
+      </div>
     );
   }
 
-}  
+}
 
 export default App;
--- src/components/Filtro/filtro.js
+++ src/components/Filtro/filtro.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import logo from '../../imagens/logo.png'
 
 
 const FiltroContainer = styled.section`
-    border: 1px solid black;
+    box-shadow: 1px 5px 5px 1px rgb(175, 172, 172);
     margin: 8px;
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    padding: 1.8rem;
+    height: 60%;
     
 `
-
+const Input = styled.input`
+    width: 70%;
+    /* border: none; */
+    outline: none;
+    /* border-bottom: 2px solid rgba(0, 0, 0, 0.8 ); */
+    height: 1.2rem;
+`
+const Select = styled.select`
+    width: 70%;
+    height: 1.2rem;
+`
 
 
 
@@ -20,47 +33,49 @@ function Filtro(props) {
 
     return (
 
+        <div>
+            <img src={logo} alt="logo da loja" className="logo-img" />
+            <FiltroContainer >
 
-        <FiltroContainer >
+                <h2>Filtro</h2>
 
-            <p>Filtro</p>
+                <label htmlFor="valorMinimo">Valor Mínimo:</label>
+                <Input
+                    name="valorMinimo"
+                    type="number"
+                    value={props.precoMinimo}
+                    onChange={props.atualizaPrecoMinimo}
+                />
 
-            <label htmlFor="valorMinimo">Valor Mínimo:</label>
-            <input
-                name="valorMinimo"
+                <label htmlFor="valorMaximo">Valor Máximo:</label>
+                <Input
+                    name="valorMaximo"
+                    type="number"
+                    value={props.precoMaximo}
+                    onChange={props.atualizaPrecoMaximo}
+                />
 
-                value={props.precoMinimo}
-                onChange={props.atualizaPrecoMinimo}
-            />
-            
-            <label htmlFor="valorMaximo">Valor Máximo:</label>
-            <input
-                name="valorMaximo"
+                <label htmlFor="nome">Busca por nome:</label>
+                <Input
+                    name="nome"
+                    value={props.busca}
+                    onChange={props.atualizaBusca}
+                />
 
-                value={props.precoMaximo}
-                onChange={props.atualizaPrecoMaximo}
-            />
-            
-            <label htmlFor="nome">Busca por nome:</label>
-            <input
-                name="nome"
-                value={props.busca}
-                onChange={props.atualizaBusca}
-            />
+                <label htmlFor="ordem">Ordenar por:</label>
+                <Select
+                    name="ordem"
+                    value={props.ordenacao}
+                    onChange={props.ordenaProdutos}
 
-            <label htmlFor="ordem">Ordenar por:</label>
-            <select
-                name="ordem"
-                value={props.ordenacao}
-                onChange={props.ordenaProdutos}
+                >
+                    <option value="maiorPreco">Maior preço</option>
+                    <option value="menorPreco">Menor preço</option>
+                    <option value="nome">Ordem Alfabética</option>
+                </Select>
 
-            >
-                <option value="maiorPreco">Maior preço</option>
-                <option value="menorPreco">Menor preço</option>
-                <option value="nome">Ordem Alfabética</option>
-            </select>
-
-        </FiltroContainer>
+            </FiltroContainer>
+        </div>
 
     );
 }
