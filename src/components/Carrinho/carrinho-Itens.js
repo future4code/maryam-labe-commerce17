14e1c6acff54f5cb54d11e314d8e89bd0c2e8f0c
Mariana Carolina de Souza <smcarolina42@gmail.com>
2021-09-20 08:09:17 -0300

    ajuste da função remover itens

--- src/components/Carrinho/carrinho-Itens.js
+++ src/components/Carrinho/carrinho-Itens.js
@@ -5,32 +5,33 @@ import ValorTotal from './carrinho-valor-total';
 
 
 
-export default function Carrinho (props){
+export default function Carrinho(props) {
 
-		return (<div>
-			<header>
+	return (<div>
+		<header>
 			<h2>Carrinho:</h2>
-			
-			
-			
+
+
+
 			{props.itensCarrinho
-			.filter((item) => {
-				return item.quantidade > 0
-			})
-			.map((item) => {
-				return (
-					<div className="containerCarrinho">
-					<p>{item.nome} R${item.preco},00</p>
-					{item.quantidade }x
-					
-					<button onClick={props.onClickCarrinho(item)}>Remover</button>
-					</div>
+				.filter((item) => {
+					return item.quantidade > 0
+				})
+				.map((item) => {
+					return (
+						<div className="containerCarrinho">
+							<p>{item.nome} R${item.preco},00</p>
+							{item.quantidade}x
+
+							<button onClick={()=>props.onClick(item)}>Remover</button>
+						</div>
+					)
+				}
 				)}
-			)} 
 			<ValorTotal
-			valor={props.valorTotal}
+				valor={props.valorTotal}
 			/>
 		</header>
-		</div>)
-	}
-	
+	</div>)
+}
+
